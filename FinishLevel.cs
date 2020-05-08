53e4d4ecc0c5784558b3dd29b0e286fc60ddb6a4
Sorina Andrei <48566916+Sonia133@users.noreply.github.com>
2020-05-08 18:48:56 +0300

    end level

---
+++ FinishLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    private int index;
+    public GameObject completeLevelUI;
+    void Start()
+    {
+        index = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "EndLevel")
+        {
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(2);
+        completeLevelUI.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+    }
+}
