f98a8f9fda9bacfee6c825f224a7d68f1e9a5f40
MadalinaVersin <50883586+MadalinaVersin@users.noreply.github.com>
2020-05-11 18:43:50 +0300

    Add files via upload

---
+++ FallingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+   Rigidbody2D rb2d;
+
+    public float fallDelay;
+    public float destroyDelay;
+ 
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Invoke("DropPlatform", fallDelay);
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+    
+   void DropPlatform()
+    {
+        rb2d.isKinematic = false;
+    }
+  
+}
---
+++ TrapolineScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapolineScript : MonoBehaviour
+{
+    bool onTop;
+
+    GameObject bouncer;
+
+    Animator anim;
+
+    public Vector2 velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (onTop)
+        {
+            anim.SetBool("isStepped", true);
+
+            bouncer = other.gameObject;
+
+        }
+
+    }
+
+    void OnTriggerEnter2D()
+    {
+        onTop = true;
+    }
+
+    void OnTriggerExit2D()
+    {
+        onTop = false;
+        anim.SetBool("isStepped", false);
+    }
+
+    void Jump()
+    {
+        bouncer.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
