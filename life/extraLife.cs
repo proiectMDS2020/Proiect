f3fabfe129599b06b4e660c2707e92e6ba2c4547
alexandrazambitchi <45661431+alexandrazambitchi@users.noreply.github.com>
2020-05-13 15:32:53 +0300

    Add files via upload

---
+++ life/extraLife.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extraLife : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            health.Health += 1;
+            Destroy(gameObject);
+        }
+    }
+}
