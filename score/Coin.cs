c0360f439e1c772df6e3d0be5ad583f4b1d588d7
alexandrazambitchi <45661431+alexandrazambitchi@users.noreply.github.com>
2020-05-13 15:32:09 +0300

    Add files via upload

---
+++ score/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int CoinValue;
+    public bool hasTouched = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(hasTouched==false)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                ScoreManager.instance.ChangeScore(CoinValue);
+                Destroy(gameObject);
+                hasTouched = true;
+            } 
+        }
+        
+    }
+}
---
+++ score/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static ScoreManager instance;
+    public GameObject text;
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+        if (health.Health>3)
+        {
+            score += 50;
+        }
+    }
+
+    public void ChangeScore(int CoinValue)
+    {
+        score += CoinValue;
+        text.GetComponent<Text>().text = "Score: " + score.ToString();
+    }
+}
