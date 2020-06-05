bd1dd6d3672c170b695cd65f7f60876c8d7a5196
MadalinaVersin <50883586+MadalinaVersin@users.noreply.github.com>
2020-06-05 18:27:45 +0300

    Add files via upload

--- platforms/FallingPlatform.cs
+++ platforms/FallingPlatform.cs
@@ -6,29 +6,48 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-   Rigidbody2D rb2d;
-
-    public float fallDelay;
-    public float destroyDelay;
- 
+  
+    Rigidbody2D rb2d;
+    public float fallDelay;//in cat timp va disparea placa
+    public static bool disabled = false;// variabila utilizata pentru a seta placa activa sau inactiva
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+       rb2d = GetComponent<Rigidbody2D>();
+    }
+     
+    void Update()
+    {
+        //setam placa activa sau inactiva in functie de valoare variabilei disabled
+        if(disabled)
+        {
+            gameObject.SetActive(false);
+        }
+        else {
+            gameObject.SetActive(true);
+        }
+
     }
 
     void OnCollisionEnter2D (Collision2D col)
     {
-        if (col.gameObject.name.Equals("Player"))
-        {
-            Invoke("DropPlatform", fallDelay);
-            Destroy(gameObject, destroyDelay);
+        //la coleziunea cu player-ul, placa va apela functia "Fall"
+        if (col.gameObject.tag.Equals("Player"))
+        { 
+           StartCoroutine(Fall());
         }
+
+
+
     }
     
-   void DropPlatform()
+    IEnumerator Fall()
     {
-        rb2d.isKinematic = false;
+        //placa asteapta numarul de secunde reprezentat de variabila "fallDelay"
+         yield return new WaitForSeconds(fallDelay);
+         disabled = true; //seteaza obiectul ca fiind inactiv
+         yield return 0;
     }
+
   
 }
