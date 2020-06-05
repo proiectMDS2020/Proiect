cab2ee52e2ea4b2577148cc16c0451fdce4bca94
MadalinaVersin <50883586+MadalinaVersin@users.noreply.github.com>
2020-06-05 18:52:18 +0300

    Add files via upload

--- platforms/TrapolineScript.cs
+++ platforms/TrapolineScript.cs
@@ -4,52 +4,49 @@ using UnityEngine;
 
 public class TrapolineScript : MonoBehaviour
 {
-    bool onTop;
+    bool onTop; //variabila utilizata pentru a vedea daca player-ul se afla sau nu deasupra trambulinei;
 
-    GameObject bouncer;
+    GameObject bouncer; //player-ul
 
-    Animator anim;
+    Animator anim; //animatia trambulinei
 
-    public Vector2 velocity;
+    public Vector2 velocity; //utilizat pentru a seta cat de tare va fi aruncat in aer player-ul;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
     }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-      
-    }
 
     void OnCollisionStay2D(Collision2D other)
     {
+        //atunci cand player-ul se afla deasupra trambulinei, setam animatia true 
         if (onTop)
         {
             anim.SetBool("isStepped", true);
 
-            bouncer = other.gameObject;
+            bouncer = other.gameObject;//setam player-ul (obiectul care va fi aruncat)
 
         }
 
     }
-
-    void OnTriggerEnter2D()
+     void OnTriggerEnter2D()
     {
+        //daca player-ul ajunge deasupra trambulinei, setam variabila onTop = true;
         onTop = true;
     }
 
+
     void OnTriggerExit2D()
-    {
+    { 
+        //cand player-ul iese de pe trambulina, oprim animatia trambulinei, setam variabila onTop ='false';
         onTop = false;
         anim.SetBool("isStepped", false);
     }
 
     void Jump()
-    {
+    {   //aplicatie utilizata pentru a arunca in aer player-ul;
         bouncer.GetComponent<Rigidbody2D>().velocity = velocity;
     }
 }
