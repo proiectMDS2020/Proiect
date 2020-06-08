41d377fa3ad0760fb917a4f891b5485d70465848
Sorina Andrei <48566916+Sonia133@users.noreply.github.com>
2020-06-08 18:59:39 +0300

    butoane plansele de win/lose

---
+++ WinLose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLose : MonoBehaviour
+{
+    // se intoarce la meniul principal
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // reia jocul de la nivelul 1
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
